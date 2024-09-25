8f4c5c8be66bf906f54faaf47f06e3e7c7b7c029
alamin899 <alaminhossen899@gmail.com>
2024-09-26 00:27:51 +0600

    [product-management] feat: Product management from admin pannel

---
+++ app/Http/Middleware/RedirectIfAdminNotAuthenticated.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class RedirectIfAdminNotAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->is('admin') && !Auth::check()) {
+            return redirect()->route('filament.admin.auth.login');
+        }
+        return $next($request);
+    }
+}
