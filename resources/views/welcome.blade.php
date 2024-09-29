04079e4231908a52cd2956118f7483c30b2cbfaf
MD.AL-AMIN <alaminhossen899@gmail.com>
2024-09-29 17:48:17 +0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,7 +83,7 @@
                     </main>
 
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
-                        Backend Task v1.0.1
+                        Backend Task v1.0.2
                     </footer>
                 </div>
             </div>
